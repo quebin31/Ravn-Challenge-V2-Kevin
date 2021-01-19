56c5a5ee48b9cbf60decd2daaf6dae554bb254b6
Kevin Del Castillo <quebin31@gmail.com>
2021-01-19 18:51:59 -0500

    Add internal variables:
    References to Views.
    List of loaded people.
    Current end cursor used in data loading.
    Apollo client.

--- app/src/main/java/co/ravn/kevin/peopleofstarwars/MainActivity.kt
+++ app/src/main/java/co/ravn/kevin/peopleofstarwars/MainActivity.kt
@@ -8,6 +8,18 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "MainActivity"
     }
 
+    private lateinit var mLoadingComp: LinearLayout
+    private lateinit var mPeopleRecyclerView: RecyclerView
+    private lateinit var mPeopleListAdapter: PeopleAdapter
+
+    private var mIsLoadingData = true
+    private var mPeopleList = mutableListOf<AllPeoplePaginatedQuery.Person>()
+    private var mCurrEndCursor: String? = null
+    private val mApolloClient = ApolloClient.builder()
+            .serverUrl("https://swapi-graphql.netlify.app/.netlify/functions/index")
+            .build()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
