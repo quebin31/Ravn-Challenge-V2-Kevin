40f0c6b9a3800af60821661c600feb0b1920c6c1
Kevin Del Castillo <quebin31@gmail.com>
2021-01-19 22:22:39 -0500

    Add on item click listener to PeopleAdapter.

--- app/src/main/java/co/ravn/kevin/peopleofstarwars/adapters/PeopleAdapter.kt
+++ app/src/main/java/co/ravn/kevin/peopleofstarwars/adapters/PeopleAdapter.kt
@@ -10,18 +10,27 @@ import co.ravn.kevin.AllPeoplePaginatedQuery
 import co.ravn.kevin.peopleofstarwars.R
 
 class PeopleAdapter(context: Context, peopleList: MutableList<AllPeoplePaginatedQuery.Person>): RecyclerView.Adapter<PeopleAdapter.PersonViewHolder>() {
-    class PersonViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class PersonViewHolder(itemView: View, adapter: PeopleAdapter): RecyclerView.ViewHolder(itemView) {
+        private val mAdapter = adapter
         val personName: TextView = itemView.findViewById(R.id.personName)
         val personFooter: TextView = itemView.findViewById(R.id.personFooter)
+
+        init {
+            itemView.setOnClickListener {
+                mAdapter.onItemClick(layoutPosition)
+            }
+        }
     }
 
     private val mContext = context
     private val mPeopleList = peopleList
     private val mInflater = LayoutInflater.from(context)
 
+    private var mOnItemClickListener: ((AllPeoplePaginatedQuery.Person) -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
         val mItemView = mInflater.inflate(R.layout.item_person, parent, false)
-        return PersonViewHolder(mItemView)
+        return PersonViewHolder(mItemView, this)
     }
 
     override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
@@ -35,5 +44,13 @@ class PeopleAdapter(context: Context, peopleList: MutableList<AllPeoplePaginated
 
     override fun getItemCount(): Int {
         return mPeopleList.size
+    }
+
+    fun onItemClickListener(listener: (AllPeoplePaginatedQuery.Person) -> Unit) {
+        mOnItemClickListener = listener
+    }
+
+    private fun onItemClick(position: Int) {
+        mOnItemClickListener?.invoke(mPeopleList[position])
     }
 }
